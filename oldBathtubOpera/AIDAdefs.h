f18e77675cf78b968f2f1eacf0b05437b72ecce4
vihophong <hophongmc@gmail.com>
2017-02-17 20:51:07 +0900

    eurica2

---
+++ oldBathtubOpera/AIDAdefs.h
@@ -0,0 +1,7 @@
+#define NumDSSD 6
+#define NumStrX 128
+#define NumStrY 128
+#define NumStrXY 256
+#define NumFee 33
+#define NumChFee 64
+#define tm_stp_scaler_ratio 4 //AIDA clock 100 MHz,Correlation scaler 25 MHz
---
+++ oldBathtubOpera/EURICA_cpp.d
@@ -0,0 +1,38 @@
+
+# DO NOT DELETE
+
+./EURICA_cpp.so: EURICA.h /home/phong/root/include/TObject.h
+./EURICA_cpp.so: /home/phong/root/include/Rtypes.h
+./EURICA_cpp.so: /home/phong/root/include/RConfig.h
+./EURICA_cpp.so: /home/phong/root/include/RVersion.h
+./EURICA_cpp.so: /home/phong/root/include/DllImport.h
+./EURICA_cpp.so: /home/phong/root/include/Rtypeinfo.h
+./EURICA_cpp.so: /home/phong/root/include/snprintf.h
+./EURICA_cpp.so: /home/phong/root/include/strlcpy.h
+./EURICA_cpp.so: /home/phong/root/include/TGenericClassInfo.h
+./EURICA_cpp.so: /home/phong/root/include/TSchemaHelper.h
+./EURICA_cpp.so: /home/phong/root/include/TStorage.h
+./EURICA_cpp.so: /home/phong/root/include/TVersionCheck.h
+./EURICA_cpp.so: /home/phong/root/include/Riosfwd.h
+./EURICA_cpp.so: /home/phong/root/include/TBuffer.h
+./EURICA_cpp.so: /home/phong/root/include/TVector3.h
+./EURICA_cpp.so: /home/phong/root/include/TError.h
+./EURICA_cpp.so: /home/phong/root/include/TVector2.h
+./EURICA_cpp.so: /home/phong/root/include/TMatrix.h
+./EURICA_cpp.so: /home/phong/root/include/TMatrixF.h
+./EURICA_cpp.so: /home/phong/root/include/TMatrixT.h
+./EURICA_cpp.so: /home/phong/root/include/TMatrixTBase.h
+./EURICA_cpp.so: /home/phong/root/include/TNamed.h
+./EURICA_cpp.so: /home/phong/root/include/TString.h
+./EURICA_cpp.so: /home/phong/root/include/TMathBase.h
+./EURICA_cpp.so: /home/phong/root/include/TMatrixFBasefwd.h
+./EURICA_cpp.so: /home/phong/root/include/TMatrixDBasefwd.h
+./EURICA_cpp.so: /home/phong/root/include/TVectorFfwd.h
+./EURICA_cpp.so: /home/phong/root/include/TVectorDfwd.h
+./EURICA_cpp.so: /home/phong/root/include/TMatrixTUtils.h
+./EURICA_cpp.so: /home/phong/root/include/TMatrixFfwd.h
+./EURICA_cpp.so: /home/phong/root/include/TMatrixFUtils.h
+./EURICA_cpp.so: /home/phong/root/include/TMatrixFUtilsfwd.h
+./EURICA_cpp.so: /home/phong/root/include/TMath.h EURICAdefs.h
+./EURICA_cpp.so: /home/phong/root/include/cintdictversion.h /home/phong/root/include/RVersion.h
+EURICA_cpp__ROOTBUILDVERSION= 5.34/36
